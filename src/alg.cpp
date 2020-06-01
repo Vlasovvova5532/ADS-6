f968e076bc926540b9a87b0a2f584401a8de7598
Vlasovvova5532 <62699318+Vlasovvova5532@users.noreply.github.com>
2020-06-01 20:42:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,43 @@
 
 BST<std::string> makeTree(char* filename)
 {
+std::string slovo;
+	std::ifstream F(filename);
+	BST<std::string> *tree = new BST<std::string>;
+	char line = ' ';
+	while (!F.eof()) {
+	
+		while ( line < 65 && (!F.eof()) ) 
+		{
+			F.get(line);
+		}
+
+while (  line >= 65  && (!F.eof()))
+{
+	if (line >= 65 && line <= 90)
+	{
+	
+		slovo += line;	
+	}
+	if (line >= 97 && line <= 122)
+	{
+		slovo += line;
+	}
+	
+	F.get(line);	
+}
+for (int i = 0; i < slovo.length(); i++)
+{
+	if (slovo[i] >= 65 && slovo[i] <= 90)
+		slovo[i] += 32;
+}
+
+(*tree).add(slovo);
+slovo = "";
+	}
+
+		
+	
+	return *tree;
 
 }
